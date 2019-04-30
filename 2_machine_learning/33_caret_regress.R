9537f94adbc3859f23ea0259dde390159840ee51
Ian Cook <icook@cloudera.com>
2019-04-30 12:52:05 +0100

    Improve heading

--- 2_machine_learning/33_caret_regress.R
+++ 2_machine_learning/33_caret_regress.R
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# # Regression model with caret
+# # Linear regression model with caret
 
 # This example demonstrates a simple regression modeling
 # task using the `lm` method with the
